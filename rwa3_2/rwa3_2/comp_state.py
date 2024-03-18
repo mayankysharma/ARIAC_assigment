0131468e66957d83d172a14f422c097fef9196fa
yashveer <yashveerjain321@gmail.com>
2024-03-18 19:48:32 -0400

    tast 3 completed

--- rwa3_2/rwa3_2/comp_state.py
+++ rwa3_2/rwa3_2/comp_state.py
@@ -11,7 +11,16 @@ from ariac_msgs.msg import (
 from std_srvs.srv import Trigger
 
 
-class AriacInterface(rclpy.node.Node):
+class AriacInterface(Node):
+    '''
+    Class for a competition interface node.
+
+    Args:
+        Node (rclpy.node.Node): Parent class for ROS nodes
+
+    Raises:
+        KeyboardInterrupt: Exception raised when the user uses Ctrl+C to kill a process
+    '''
 
     _competition_states = {
         CompetitionStateMsg.IDLE: 'idle',
@@ -20,12 +29,13 @@ class AriacInterface(rclpy.node.Node):
         CompetitionStateMsg.ORDER_ANNOUNCEMENTS_DONE: 'order_announcements_done',
         CompetitionStateMsg.ENDED: 'ended',
     }
+    '''Dictionary for converting CompetitionState constants to strings'''
 
-    comp_state_topic_name = "/ariac/competition_state"
-    start_comp_service_name = "/ariac/start_competition"
+    topic_name = "/ariac/competition_state"
+    service_name = "/ariac/start_competition"
 
-    def __init__(self, node_name):
-        super(AriacInterface, self).__init__(node_name)
+    def __init__(self):
+        super().__init__('competition_interface')
 
         sim_time = Parameter(
             "use_sim_time",
@@ -34,74 +44,72 @@ class AriacInterface(rclpy.node.Node):
         )
 
         self.set_parameters([sim_time])
+        # Service client for starting the competition
+        self._start_competition_client = self.create_client(
+            Trigger, AriacInterface.service_name)
 
-        self.sub = self.create_subscription(
+        # Subscriber to the competition state topic
+        self._competition_state_sub = self.create_subscription(
             CompetitionStateMsg,
-            AriacInterface.comp_state_topic_name,
-            self.comp_callback,
+            AriacInterface.topic_name,
+            self._competition_state_cb,
             10)
-        
-        self._comp_state = None
-        self._comp_started=False
 
-        self._start_comp_client = self.create_client(Trigger, AriacInterface.start_comp_service_name)
+        # Store the state of the competition
+        self._competition_state: CompetitionStateMsg = None
 
-
-    def comp_callback(self, msg):
-        if self._comp_state != msg.competition_state:
+    def _competition_state_cb(self, msg: CompetitionStateMsg):
+        f'''Callback for the topic {AriacInterface.topic_name}
+        Arguments:
+            msg -- CompetitionState message
+        '''
+        # Log if competition state has changed
+        if self._competition_state != msg.competition_state:
             state = AriacInterface._competition_states[msg.competition_state]
-            self.get_logger().info(f"State Changed to {state}")
-            self._comp_state = msg.competition_state
-        if self._comp_started:
+            self.get_logger().info(f'Competition state is: {state}', throttle_duration_sec=1.0)
+
+        self._competition_state = msg.competition_state
+
+    def start_competition(self):
+        '''Function to start the competition.
+        '''
+        self.get_logger().info('Waiting for competition to be ready')
+
+        if self._competition_state == CompetitionStateMsg.STARTED:
             return
-        while self._comp_state != "ready":
+        # Wait for competition to be ready
+        while self._competition_state != CompetitionStateMsg.READY:
             try:
                 rclpy.spin_once(self)
             except KeyboardInterrupt:
                 return
-        self._comp_started = True
-        # if not self._comp_started and self._comp_state == "ready":
-        self.start_competition() # to start the competition
 
-    def start_competition(self):
+        self.get_logger().info('Competition is ready. Starting...')
 
-        while not self._start_comp_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("Service not available, waiting...")
+        # Check if service is available
+        if not self._start_competition_client.wait_for_service(timeout_sec=3.0):
+            self.get_logger().error(f'Service \'{AriacInterface.service_name}\' is not available.')
+            return
 
-        
-        self._request= Trigger.Request()
+        # Create trigger request and call starter service
+        request = Trigger.Request()
+        future = self._start_competition_client.call_async(request)
 
-        # Call the service asynchronously
-        future = self._start_comp_client.call_async(self._request) # request is empty just need a response.
+        # Wait until the service call is completed
+        rclpy.spin_until_future_complete(self, future)
 
-        # Add a callback function to be called when the future is complete
-        future.add_done_callback(self.wait_comp_callback)
-
-    def wait_comp_callback(self, future):
-        """
-        Callback function for the future object
-
-        Args:
-            future (Future): A future object
-        """
-        details = future.result()
-        if details.success:
-            self.get_logger().info(f"Successfully Started!!")
+        if future.result().success:
+            self.get_logger().info('Started competition.')
         else:
-            self.get_logger().info(f"{details.message}")
-   
+            self.get_logger().warn('Unable to start competition')
+
+
 
 def main(args=None):
     rclpy.init(args=args)
-
-    comp_state_sub = AriacInterface("ariac_interface")
-
-    rclpy.spin(comp_state_sub)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    comp_state_sub.destroy_node()
+    interface = AriacInterface()
+    interface.start_competition()
+    interface.destroy_node()
     rclpy.shutdown()
 
 
