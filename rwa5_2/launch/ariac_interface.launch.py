6b5f59a0be31518353e86c0684bc6ba1aed715f8
yashveer <yashveerjain321@gmail.com>
2024-05-07 23:15:20 -0400

    updated file names and added README

--- rwa5_2/launch/ariac_interface.launch.py
+++ rwa5_2/launch/ariac_interface.launch.py
@@ -1,27 +1,75 @@
+import os
+import yaml
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+)
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-# from ariac_moveit_config.parameters import generate_parameters
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ariac_moveit_config.parameters import generate_parameters
 
 
-def generate_launch_description():
-    '''
-    Generates a LaunchDescription for launching the ARIAC interface node.
+def launch_setup(context, *args, **kwargs):
+    """
+    Function to set up the launch configuration for moving the robot.
+
+    Args:
+        context (LaunchContext): The launch context.
+        *args: Additional positional arguments.
+        **kwargs: Additional keyword arguments.
 
     Returns:
-        LaunchDescription: The LaunchDescription instance.
-    '''
-    ld = LaunchDescription()
-    # Interface node
-    rwa4_interface = Node(
+        List[Node]: A list of nodes to start for moving the robot.
+    """
+    demo_cpp = Node(
+        package="rwa5_2",
+        executable="robot_ariac",
+        output="screen",
+        parameters=generate_parameters()+[{"use_sim_time":True}],
+    )
+
+    moveit = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                FindPackageShare("ariac_moveit_config"),
+                "/launch",
+                "/ariac_robots_moveit.launch.py",
+            ]
+        )
+    )
+    
+    rwa5_interface = Node(
         package="rwa5_2",
         executable="ariac_interface_main.py",
         name="rwa5_interface",
         output="screen",
         # parameters=generate_parameters()
     )
+    
+    nodes_to_start = [
+        demo_cpp,
+        moveit,
+        rwa5_interface
+    ]
 
-    ld.add_action(rwa4_interface)
-    return ld  # Return LaunchDescription instance
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "rviz", default_value="false", description="start rviz node?"
+        )
+    )
+
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
